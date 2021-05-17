02866acb78d729574825bce63fc1a430f3d080d4
Abhay <abhay199658@gmail.com>
2021-05-17 21:55:26 +0530

    [add] config file

---
+++ config.js
@@ -0,0 +1,15 @@
+exports.config = {
+  email: {
+    from: "",
+    to: [],
+    auth: {
+      user: "",
+      pass: "",
+      host: "",
+      port: 587
+    },
+  },
+  filter: {
+    pin: ""
+  },
+};
